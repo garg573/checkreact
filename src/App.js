458ade77e0b89ed8594e49e5e278f0fc1975a110
garg573 <gauravgarg573@gmail.com>
2022-11-01 00:26:51 +0530

    second updated commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           React Is gaurav Website
         </a>
       </header>
     </div>
